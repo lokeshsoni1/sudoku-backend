bc6feb0e2af1892c5005ecb5eb8717237a7694cc
Lokesh Soni <verrmaji001@gmail.com>
2026-03-29 12:08:21 +0530

    Update HelloController.java

--- src/main/java/com/lokesh/sudoku/HelloController.java
+++ src/main/java/com/lokesh/sudoku/HelloController.java
@@ -3,7 +3,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HelloController {
     @GetMapping("/hello")
